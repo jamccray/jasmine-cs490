fb4eb57c660747f325e8d34abf493cdf28f55d95
jamccray <jamccray@coastal.edu>
2014-10-23 10:33:33 -0400

    Updated SQL
    created foreign keys and still making more tables to join on

--- all the sql.sql
+++ all the sql.sql
@@ -1,3 +1,10 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+use fooddb;
+
 -- users table ; userId is like our student Id (but all users arent students)
 drop table if exists onlineforms_user;
 
@@ -7,11 +14,22 @@ userName varchar(15) not null,
 userFirstName varchar(30) not null,
 userLastName varchar(30) not null,
 userEmail varchar(30) not null,
-userId varchar(10) not null,
+userId varchar(10) unique not null,
 userPassword varchar(20) not null,
 inactive boolean,
 primary key(id)
 );
+
+
+insert into onlineforms_user (userName, userFirstName, userLastName, userEmail, userId, userPassword, inactive) values ('cjmack', 'Charles', 'Mack', 'cjmack@g.coastal.edu', '0123456', '13243254',FALSE);
+
+insert into onlineforms_user (userName, userFirstName, userLastName, userEmail, userId, userPassword, inactive) values ('jmccray', 'Jasmine', 'McCray', 'jmccray@g.coastal.edu', '0987654','ads8er94',FALSE);
+
+insert into onlineforms_user (userName, userFirstName, userLastName, userEmail, userId, userPassword, inactive) values ('aatay', 'Aydin', 'Atay', 'aatay@g.coastal.edu', '0993723', '0aojdfo4',FALSE);
+
+insert into onlineforms_user (userName, userFirstName, userLastName, userEmail, userId, userPassword, inactive) values ('badams', 'Bobby', 'Adams', 'badams@g.coastal.edu','9998765','ha598449',FALSE);
+
+SELECT * from onlineforms_user;
 
 -- authenticated session; users are added to this table when they succesfully log in
 drop table if exists authenticated_Session; 
@@ -23,12 +41,21 @@ userPassword varchar(20) not null,
 userId varchar(10) not null,
 TS timestamp,
 session_id varchar(60) not null,
-primary key(id)
+onlineforms_user_id int not null,
+primary key(id),
+INDEX `fk_authSession_users1_idx` (`onlineforms_user_id` ASC) ,
+CONSTRAINT `fk_authSession_users1`
+FOREIGN KEY (`onlineforms_user_id`)
+    REFERENCES `onlineforms_user` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
 select * from authenticated_Session;
 
 -- forms table; all avaliable forms
+drop table if exists onlineforms_forms;
+
 create table onlineforms_forms (
 id int not null auto_increment,
 title varchar (60) not null,
@@ -43,6 +70,27 @@ primary key(id)
 drop table if exists users_forms;
 
 CREATE  TABLE users_forms (
-  `onlineforms_user_id` INT NOT NULL ,
-  `forms_id` INT NOT NULL ,
-  PRIMARY KEY (`onlineforms_user_id`, `forms_id`) );
+  user_id INT NOT NULL ,
+  forms_id INT NOT NULL ,
+  PRIMARY KEY (`user_id`, `forms_id`),
+INDEX `fk_users_forms_users1_idx` (`user_id` ASC) ,
+CONSTRAINT `fk_users_forms_users1`
+  FOREIGN KEY (`user_id`)
+    REFERENCES `onlineforms_user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+INDEX `fk_users_forms_forms1_idx` (`forms_id` ASC) ,
+CONSTRAINT `fk_users_forms_forms1`
+  FOREIGN KEY (`forms_id`)
+    REFERENCES `onlineforms_forms` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+select * from users_forms;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET FOREIGN_KEY_CHECKS=1;
